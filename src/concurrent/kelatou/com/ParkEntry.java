e92b99790de79f8f5d9ecd1c6de6595f1b62223f
HanChen <hncen@163.com>
2017-01-02 17:27:56 +0800

    improve

--- src/concurrent/kelatou/com/ParkEntry.java
+++ src/concurrent/kelatou/com/ParkEntry.java
@@ -11,171 +11,189 @@ import java.util.stream.Collectors;
 
 public class ParkEntry {
 
-    // the number limit of visitors
-    public static int MAX_ENTRY = 50;
+	// the number limit of visitors
+	public static int MAX_ENTRY = 50;
 
-    final static AtomicInteger seq = new AtomicInteger();
+	final static AtomicInteger seq = new AtomicInteger();
 
-    public static void main(String[] args) throws Exception {
-        if (args.length > 0)
-            MAX_ENTRY = new Integer(args[0]);
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0)
+			MAX_ENTRY = new Integer(args[0]);
 
-        CountDownLatch cldComer = new CountDownLatch(1);
-        CountDownLatch cldEnter = new CountDownLatch(1);
+		CountDownLatch cldComer = new CountDownLatch(4);
 
-        ExecutorService exec = Executors.newCachedThreadPool();
+		ExecutorService exec = Executors.newCachedThreadPool();
 
-        // 4 park doors
-        Door[] doors = { new Door("EAST", seq, exec, cldEnter), new Door("WEST", seq, exec, cldEnter),
-                new Door("SOUTH", seq, exec, cldEnter), new Door("NORTH", seq, exec, cldEnter) };
+		// 4 park doors
+		Door[] doors = { new Door("EAST"), new Door("WEST"), new Door("SOUTH"), new Door("NORTH") };
 
-        Random randDoor = new Random(100);
+		// Simulate coming visitors who want to enter park
+		for (Door door : doors) {
+			exec.execute(new VisitorsCome(door, seq, cldComer, MAX_ENTRY));
+		}
 
-        // Simulate coming vistors who want to enter park
-        exec.execute(new Runnable() {
-            @Override
-            public void run() {
-                VistorGenerator vistorGenerator = new VistorGenerator();
-                while (!Thread.interrupted()) {
-                    if (seq.get() < MAX_ENTRY) {
-                        doors[randDoor.nextInt(doors.length)].vistorComes(vistorGenerator.next());
-                    } else {
-                        cldComer.countDown();
-                    }
-                }
-            }
-        });
+		// Simulate visitor is allowed to enter the park
+		for (Door door : doors) {
+			exec.execute(new visitorsEnter(door, seq, MAX_ENTRY));
+		}
 
-        // Simulate vistor is allowed to enter the park
-        exec.execute(new Runnable() {
+		// print name list who are still waiting at the doors
+		exec.execute(new Runnable() {
+			@Override
+			public void run() {
 
-            @Override
-            public void run() {
-                while (!Thread.interrupted()) {
-                    doors[randDoor.nextInt(doors.length)].checkAndAllowToEnter();
-                }
-            }
-        });
+				try {
+					cldComer.await();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
-        exec.execute(new Runnable() {
-            @Override
-            public void run() {
+				for (Door door : doors) {
+					door.showWaitingVisitorsInfo();
+				}
 
-                try {
-                    cldEnter.await();
-                    cldComer.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+				exec.shutdownNow();
+			}
+		});
+	}
+}
 
-                for (Door door : doors) {
-                    door.showWaitingVisitorsInfo();
-                }
+class VisitorsCome implements Runnable {
+	final Door door;
+	final CountDownLatch cldComer;
+	final AtomicInteger seq;
+	final int maxLimit;
 
-                exec.shutdownNow();
-            }
-        });
-    }
+	VisitorsCome(final Door door, final AtomicInteger seq, final CountDownLatch cldComer, final int maxLimit) {
+		this.door = door;
+		this.cldComer = cldComer;
+		this.seq = seq;
+		this.maxLimit = maxLimit;
+	}
+
+	@Override
+	public void run() {
+		VistorGenerator vistorGenerator = new VistorGenerator();
+		while (seq.get() < maxLimit) {
+			door.vistorComes(vistorGenerator.next());
+
+		}
+		cldComer.countDown();
+	}
+}
+
+class visitorsEnter implements Runnable {
+	final Door door;
+	final AtomicInteger seq;
+	final int maxLimit;
+
+	visitorsEnter(final Door door, final AtomicInteger seq, final int maxLimit) {
+		this.door = door;
+		this.seq = seq;
+		this.maxLimit = maxLimit;
+	}
+
+	@Override
+	public void run() {
+		while (seq.get() < maxLimit) {
+			if (door.isVistsitorInQueue()) {
+				int newSequence = seq.incrementAndGet();
+				if (newSequence <= maxLimit) {
+					door.visitorEnters(newSequence);
+				}
+			}
+		}
+	}
 }
 
 class Door {
-    private String name;
-    private final ConcurrentLinkedQueue<Visitor> waitingQueue = new ConcurrentLinkedQueue<Visitor>();
-    private AtomicInteger seq;
-    final ExecutorService exec;
-    final CountDownLatch cld;
+	private String name;
+	private final ConcurrentLinkedQueue<Visitor> waitingQueue = new ConcurrentLinkedQueue<Visitor>();
 
-    public Door(final String name, final AtomicInteger seq, final ExecutorService exec, final CountDownLatch cld) {
-        this.name = name;
-        this.seq = seq;
-        this.exec = exec;
-        this.cld = cld;
-    }
+	public Door(final String name) {
+		this.name = name;
+	}
 
-    public void vistorComes(Visitor visitor) {
-        waitingQueue.offer(visitor);
-        System.out.println(String.format("Vistor %s comes to door %s in order to enter park", visitor.getName(), name));
-    }
+	public void vistorComes(Visitor visitor) {
+		waitingQueue.offer(visitor);
+		System.out.println(String.format("Vistor %s comes to door %s in order to enter park", visitor.getName(), name));
+	}
 
-    public void checkAndAllowToEnter() {
-        if (seq.get() >= ParkEntry.MAX_ENTRY) {
-            cld.countDown();
-            return;
-        }
+	public void visitorEnters(int sequence) {
+		if (waitingQueue.peek() != null) {
+			System.out.println(String.format("Vistor %s is allowed to enter pard from door %s, with SEQ%d",
+					waitingQueue.poll().getName(), name, sequence));
+		}
+	}
 
-        if (waitingQueue.peek() != null) {
-            if (seq.incrementAndGet() <= ParkEntry.MAX_ENTRY) {
-                System.out.println(String.format("Vistor %s is allowed to enter pard from door %s, with SEQ%d",
-                        waitingQueue.poll().getName(), name, seq.get()));
-            }
-        }
-    }
+	public void showWaitingVisitorsInfo() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(String.format("%d visitors are still waiting at door %s:", waitingQueue.size(), name));
 
-    public void showWaitingVisitorsInfo() {
-        StringBuffer sb = new StringBuffer();
-        sb.append(String.format("%d visitors are still waiting at door %s:", waitingQueue.size(), name));
+		// Since 1.8
+		String vistors = waitingQueue.stream().map(i -> i.getName()).collect(Collectors.joining(", "));
+		sb.append(vistors);
 
-        // Since 1.8
-        String vistors = waitingQueue.stream()
-                .map(i -> i.getName())
-                .collect(Collectors.joining(", "));
-        sb.append(vistors);
+		System.out.println(sb.toString());
+	}
 
-        System.out.println(sb.toString());
-    }
+	public boolean isVistsitorInQueue() {
+		return waitingQueue.peek() != null;
+	}
+
 }
 
 class VistorGenerator implements Generator<Visitor> {
 
-    private static Sex[] sexArray = { Sex.FEMALE, Sex.MALE };
+	private static Sex[] sexArray = { Sex.FEMALE, Sex.MALE };
 
-    Random rand = new Random(100);
+	Random rand = new Random(100);
 
-    @Override
-    public Visitor next() {
-        try {
-            Visitor vistor = Visitor.class.newInstance();
-            vistor.setName(UUID.randomUUID().toString());
-            vistor.setSex(sexArray[rand.nextInt(sexArray.length)]);
-            vistor.setAge(rand.nextInt(100));
-            return vistor;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Override
+	public Visitor next() {
+		try {
+			Visitor vistor = Visitor.class.newInstance();
+			vistor.setName(UUID.randomUUID().toString());
+			vistor.setSex(sexArray[rand.nextInt(sexArray.length)]);
+			vistor.setAge(rand.nextInt(100));
+			return vistor;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
 
 enum Sex {
-    MALE, FEMALE
+	MALE, FEMALE
 }
 
 class Visitor {
 
-    private String name;
-    private Sex sex;
-    private int age;
+	private String name;
+	private Sex sex;
+	private int age;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Sex getSex() {
-        return sex;
-    }
+	public Sex getSex() {
+		return sex;
+	}
 
-    public void setSex(Sex sex) {
-        this.sex = sex;
-    }
+	public void setSex(Sex sex) {
+		this.sex = sex;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
